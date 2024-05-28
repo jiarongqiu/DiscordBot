e43daaa27544c1853ef3a830a5663ea19a9e763e
jiarongqiu <jiarong_qiu>
2024-05-28 22:57:16 +0800

    qa

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ async def tldr(ctx,inputs: discord.Option(str)):
     await ctx.defer()  # 延迟响应，告诉 Discord 正在处理
     print(f"QJR ctx: {ctx} inputs: {inputs}")
     response = api.get_answer(inputs)
-    answer = ""
+    answer = "Question: {inputs}\nAnswer: "
     for text in response:
         answer += text.decode('utf-8')
     await ctx.followup.send(answer)  # 使用 followup 发送最终消息
